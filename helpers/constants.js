fe2d94ff2c6bfbae92f2508a04d954b7419b029d
Navarjun <navarjunsingh008@gmail.com>
2018-06-05 15:08:54 -0400

    fixed month name bug

--- helpers/constants.js
+++ helpers/constants.js
@@ -4,6 +4,6 @@ mod.DB_ERROR = 'DB Error';
 mod.PAGE_SIZE = 10; // page size counts from 0
 mod.PAGE_SIZE_BIG = 50;
 mod.weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
-mod.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'];
+mod.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 module.exports = mod;
